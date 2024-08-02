a4a9264d928bf9cf5cf9e0113327ecd1ec3cdde6
chinmaya <chinmaya@gmail.com>
2024-08-02 23:45:20 +0530

    initial commit

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,6 @@
 #!/usr/bin/env groovy
 
-def call() {
-    echo 'Building and push Image..'
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t chinmayapradhan/java-mysql-app:1.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push chinmayapradhan/java-mysql-app:1.0'
-    }
+def call(String imageName) {
+    echo 'building the docker image..'
+    sh "docker build -t $imageName ."
 }
