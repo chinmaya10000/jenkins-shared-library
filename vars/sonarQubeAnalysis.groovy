56e358fe996aa9ab3fa18dc5cf70abdfd97494cb
chinmaya <chinmaya@gmail.com>
2024-09-14 21:44:32 +0530

    initial commit

--- vars/sonarQubeAnalysis.groovy
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env groovy
-
-def call(String SonarQubeAPI, String Projectname, String ProjectKey) {
-    withSonarQubeEnv("${SonarQubeAPI}"){
-      sh "$SONAR_HOME/bin/sonar-scanner -Dsonar.projectName=${Projectname} -Dsonar.projectKey=${ProjectKey} -X"
-    }
-}
