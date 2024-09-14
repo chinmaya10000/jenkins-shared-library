68c2afa503cd7496595d790ae41e773a96cb8703
chinmaya <chinmaya@gmail.com>
2024-09-14 21:39:11 +0530

    initial commit

--- vars/dependencyCheck.groovy
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env groovy
-
-def call(String additionalArguments, String odcInstallation) {
-    dependencyCheck additionalArguments: additionalArguments, odcInstallation: odcInstallation
-}
