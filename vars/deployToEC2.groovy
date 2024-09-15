0d463619a14f4d0f3fc2d9a73f834cd2d40b50f7
chinmaya <chinmaya@gmail.com>
2024-09-15 19:18:36 +0530

    deploy to ec2

--- vars/deployToEC2.groovy
+++ vars/deployToEC2.groovy
@@ -1,9 +1,13 @@
 def call(String ec2Instance, String imageVersion, String dockerCredsId, String ec2KeyId) {
     withCredentials([usernamePassword(credentialsId: 'dockerCredsId', passwordVariable: 'DOCKER_USER', usernameVariable: 'DOCKER_PASS')]) {
-        sh "scp -o StrictHostKeyChecking=no server-cmds.sh ${ec2Instance}:/home/ec2-user"
-        sh "scp -o StrictHostKeyChecking=no docker-compose.yaml ${ec2Instance}:/home/ec2-user"
+        sshagent([ec2KeyId]) {
+            // Copy the necessary files to the EC2 instance
+            sh "scp -o StrictHostKeyChecking=no server-cmds.sh ${ec2Instance}:/home/ec2-user"
+            sh "scp -o StrictHostKeyChecking=no docker-compose.yaml ${ec2Instance}:/home/ec2-user"
 
-        def shellCmd = "bash ./server-cmds.sh ${imageVersion} ${DOCKER_USER} ${DOCKER_PASS}"
-        sh "ssh -o StrictHostKeyChecking=no ${ec2Instance} '${shellCmd}'"
+            // Run the deployment command on the EC2 instance
+            def shellCmd = "bash ./server-cmds.sh ${imageVersion} ${DOCKER_USER} ${DOCKER_PASS}"
+            sh "ssh -o StrictHostKeyChecking=no ${ec2Instance} '${shellCmd}'"
+        }
     }
 }
