fafe65a13517d984dc6a93cd0843f490d26bbe65
chinmaya <chinmaya@gmail.com>
2024-09-15 19:30:47 +0530

    deploy to ec2

--- vars/deployToEC2.groovy
+++ vars/deployToEC2.groovy
@@ -1,5 +1,5 @@
 def call(String ec2Instance, String imageVersion, String dockerCredsId, String ec2KeyId) {
-    withCredentials([usernamePassword(credentialsId: 'dockerCredsId', passwordVariable: 'DOCKER_USER', usernameVariable: 'DOCKER_PWD')]) {
+    withCredentials([usernamePassword(credentialsId: dockerCredsId, passwordVariable: 'DOCKER_USER', usernameVariable: 'DOCKER_PWD')]) {
         sshagent([ec2KeyId]) {
             // Copy the necessary files to the EC2 instance
             sh "scp -o StrictHostKeyChecking=no server-cmds.sh ${ec2Instance}:/home/ec2-user"
