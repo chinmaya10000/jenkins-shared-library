c16cdba8ee41eb5cc0f27b8dac339099618d8f59
chinmaya <chinmaya@gmail.com>
2024-10-18 22:05:01 +0530

    fix quality gate

--- vars/qualityGateCheck.groovy
+++ vars/qualityGateCheck.groovy
@@ -1,7 +1,6 @@
 #!/usr/bin/env groovy
 
-def call(int timeoutHours = 1, boolean abortPipeline = false) {
-    timeout(time: timeoutHours, unit: 'HOURS') {
-        waitForQualityGate abortPipeline: abortPipeline
-    }
+def call() {
+    timeout(time: 1, unit: "MINUTES") {
+        waitForQualityGate abortPipeline: false
 }
