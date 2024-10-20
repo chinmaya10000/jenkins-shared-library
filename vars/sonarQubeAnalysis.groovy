c4db49dc1053e44bc5e96e7618dbbf4af372c190
chinmaya <chinmaya@gmail.com>
2024-10-20 11:15:21 +0530

    add -Dsonar.java.binaries

--- vars/sonarQubeAnalysis.groovy
+++ vars/sonarQubeAnalysis.groovy
@@ -2,6 +2,6 @@
 
 def call(String sonarServer, String projectKey, String projectName) {
     withSonarQubeEnv(sonarServer) {
-        sh "$SCANNER_HOME/bin/sonar-scanner -Dsonar.projectKey=$projectKey -Dsonar.projectName=$projectName"
+        sh "$SCANNER_HOME/bin/sonar-scanner -Dsonar.projectKey=$projectKey -Dsonar.projectName=$projectName -Dsonar.java.binaries=target"
     }
 }
